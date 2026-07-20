0db2628fbb7879a79ee5c750c4c1c8c00cbe9afb
Arthur Dell Agnolo <arthurdellagnolo6@gmail.com>
2026-07-20 18:37:03 -0300

    Implementado o findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,18 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		LocalDate ld = LocalDate.now();
-			
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", ld, 3000., obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 }
